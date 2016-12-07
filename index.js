23e7f989dd22c0683772aecc789be68fc422dbcd
Adina Rubinoff <adinarubinoff@gmail.com>
2016-12-07 18:30:33 -0500

    adding angular

---
+++ index.js
@@ -0,0 +1,18 @@
+(function() {
+  var app = angular.module( 'spells', [] );
+
+  app.controller( 'spellCtrl', function( $scope, $http ) {
+    $scope.spellname = "A spell to rid yourself of hunger";
+    $scope.ingredients = [
+      "3 pinches of salt",
+      "The foot of a spider",
+      "1 small glass of water"
+    ];
+    $scope.special = "Perform while sitting alone in a room, preferably seated on the floor.";
+    $scope.steps = [
+      "Mix the salt and spider leg into the water.",
+      "Drink without breathing."
+    ];
+  });
+
+})();
